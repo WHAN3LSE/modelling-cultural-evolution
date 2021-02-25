7af8b6ae3cf498b42ef0aeba25e591c94538a1be
Wenjie Han <48333549+WHAN3LSE@users.noreply.github.com>
2021-02-25 11:48:32 +0100

    lesson 4

--- L3 EXERCISE.R
+++ L3 EXERCISE.R
@@ -1,19 +1,24 @@
 #Load Library
 library(lme4)
 
-#mixed effects linear regression
-plot(ysimp ~ xsimp,pch=rep(16:19,each=40),col=group, data=simp)
-summary(lm(ysimp ~ xsimp, data=simp))
-abline(lm(ysimp ~ xsimp, data=simp), col = "blue")
+plot(ysimp ~ xsimp, data=simp, col=group, pch=16)
+summary(lm(ysimp~xsimp, data=simp))
+lmList(ysimp~xsimp|group, data=simp)
+simp.model1 <- lmer(ysimp~xsimp + (1|group), 
+                    data=simp)
+summary(simp.model1)
+4.911/(4.911+1.421)
+simp.null <- lmer(ysimp ~ (1|group), data=simp)
+anova(simp.model1, simp.null, test= "chisq")
 
-#separate regression lines per group 
-linear.models <- lmList(ysimp ~ xsimp|group,data=simp)
-summary(linear.models)
+simp.model2 <- lmer(ysimp~xsimp + (1 + xsimp|group), data=simp)
+summary(simp.model2)
 
-#random intercepts only
-group.int <- lmer(ysimp ~ xsimp+(1|group), data=simp)
-group.null <- lmer(ysimp ~ (1|group), data=simp)
-anova(group.int, group.null)
+#Considering Random Effects in the Model 
+mode2.null <- lmer(ysimp~ (1 + xsimp|group), 
+                   data=simp)
+anova(simp.model2, mode2.null, test= "chisq")
 
-summary(group.int)
-coef(group.int)
+#Significance disappears - means if you look as a group of the fixed
+#effect there is a Trend but accounting for individual effects the significance
+#is not there
---
+++ L4+code.r
@@ -0,0 +1,64 @@
+#model 1: smoking and obesity
+attach(hypertension)
+model.hyper <- glm(hypnonhyp ~ smoking+obesity, binomial, data= hypertension)
+summary(model.hyper)
+step(model.hyper)
+
+#model 2: obesity
+model.hyper2 <- glm(hypnonhyp ~ obesity, binomial)
+summary(model.hyper2)
+
+#95% CI
+confint(model.hyper)
+confint.default(model.hyper)
+
+#odds ratios, probabilities
+coef(model.hyper)
+exp(cbind('Odds ratio'=coef(model.hyper), confint(model.hyper)))
+
+coef(model.hyper2)
+exp(coef(model.hyper2))
+exp(-1.676+0.76)
+1/(1+exp(-(-1.676)))
+1/(1+exp(-(-1.676+0.76)))
+
+#menarche analysis
+
+#model menarche
+model.menar <- glm(menarche~age,binomial, data=menar)
+summary(model.menar)
+#logits
+predict(model.menar)
+#probabilities
+probs <- predict(model.menar, type="response")
+probs
+#probability plot
+plot(probs~age, data=menar, pch=16, cex=0.7, ylab="menarche")
+
+#plot with frequencies by age
+ageintervals <- cut(menar$age, c(8,9,10,11,12,13,14,15,16,17,18,19,20))
+table1 <- table(ageintervals,menar$menarche)
+table1
+table2 <- prop.table(table1,1)[,2]
+table2
+points(table2 ~ c(8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5,19.5), 
+       pch=1,cex=1.5)
+#adding menarche by age
+points(menarche ~ age, col='darkgrey', data=menar)
+
+#factors with >2 levels
+model.infant <- glm(healthy ~ as.factor(month),binomial, data=infant)
+summary(model.infant)
+coef(model.infant)
+exp(coef(model.infant))
+
+
+#interactions
+model.chd <- glm(chd~age*cat*chl,binomial, data=evans)
+summary(model.chd)
+
+#model optimisation
+model.menar2 <- glm(menarche~age*igf1,binomial, data=menar)
+summary(model.menar2)
+step(model.menar2)
+summary(step(model.menar2))
